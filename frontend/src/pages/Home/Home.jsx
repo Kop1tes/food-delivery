2367e44fcc4efd83b965a7e2df09791c7c0372bf
Alexandr Prikhodko <prsasha0@gmail.com>
2024-05-18 16:11:57 +0300

    add FoodItem and FoodDisplay files

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState} from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
 
 const Home = () => {
   const [category, setCategory] = useState("All")
@@ -9,7 +10,8 @@ const Home = () => {
   return (
     <div>
       <Header />
-      <ExploreMenu category={category } setCategory={setCategory} />
+      <ExploreMenu category={category} setCategory={setCategory} />
+      <FoodDisplay category={category} />
     </div>
   )
 }
