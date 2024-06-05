54c91471423b816225580c949850f3e392338af2
Alexandr Prikhodko <prsasha0@gmail.com>
2024-06-05 15:31:28 +0300

    add few backend and StoreContext for frontend changes

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,10 @@
+import orderModel from "../models/orderModel.js";
+import userModdel from "../models/userModel.js";
+import stripe from "stripe"
+
+//placing user order for frontend
+const placeOrder = async (req, res) => {
+
+}
+
+export {placeOrder}
