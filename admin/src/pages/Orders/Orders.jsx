9df5560e41f4d1cd409e2523870d84e9eb463ebd
Alexandr Prikhodko <prsasha0@gmail.com>
2024-06-18 13:42:46 +0300

    add finals changes

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -19,6 +19,17 @@ const Orders = ({ url }) => {
     }
   }
 
+  const statusHandler = async (e, orderId) => {
+    const response = await axios.post(url + "/api/order/status", {
+      orderId,
+      status: e.target.value
+    })
+
+    if (response.data.success) {
+      await fetchAllOrders();
+    }
+  } 
+
   useEffect(() => {
     fetchAllOrders();
   }, [])
@@ -34,13 +45,26 @@ const Orders = ({ url }) => {
               <p className='order-item-food'>
                 {order.items.map((item, index) => {
                   if (index===order.items.length-1) {
-                    return item.name + " x " +item.quantity
+                    return item.name + " x " + item.quantity
                   } else {
-                    return item.name + " x " +item.quantity + ", "
+                    return item.name + " x " + item.quantity + ", "
                   }
                 })}
               </p>
+              <p className='order-item-name'>{order.address.firstName + " " + order.address.lastName}</p>
+              <div className="order-item-address">
+                <p>{order.address.street + ","}</p>
+                <p>{order.address.city+", "+order.address.state+", "+order.address.country+", "+order.address.zipcode}</p>
+              </div>
+              <p className='order-item-phone'>{order.address.phone}</p>
             </div>
+            <p>Items: {order.items.length}</p>
+            <p>${order.amount}</p>
+            <select onChange={(e)=>statusHandler(e, order._id)} value={order.status}>
+              <option value="Food Processing">Food Processing</option>
+              <option value="Out for delivery">Out for delivery</option>
+              <option value="Delivered">Delivered</option>
+            </select>
           </div>
         ))}
       </div>
